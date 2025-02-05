9966b60bbadf388639d534ceaa6733340fdc2080
Aftab Shaikh <aftab.shaikh@1Rintern.local>
2025-02-05 16:08:18 +0530

    c

---
+++ build-script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+#check for node runtime
+check_node() {
+    if ! command -v node &> /dev/null; then
+        echo "Node is not installed"
+    else 
+        node --version
+        echo "Node is installed on the system."
+    fi
+}
+
+echo "Checking Node "
+check_node
+
+echo "Installing Dependencies"
+npm install
+
+echo "deleting the old dist folder"
+rm -rf dist
+
+echo "Building the project with default configuration"
+npm run build
+
+echo "Build created Sucessfully. 👍"
